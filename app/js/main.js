4c3b6a8e7911734769607b15edde5384796f5dcf
Affectal <aff.p@yandex.ru>
2020-04-11 20:20:11 +0700

    end header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,36 @@
-$(function(){
+$('.menu-item.home').on('click', function() {
+    $('.image-room').hide();
+    $('.drop-down__inner.favourites').hide();
+    $('.drop-down__inner.search').hide();
+    $('.drop-down__inner.warehouse').show();
+});
 
+$('.menu-item.favourites').on('click', function() {
+    $('.image-room').hide();
+    $('.drop-down__inner.warehouse').hide();
+    $('.drop-down__inner.search').hide();
+    $('.drop-down__inner.favourites').show();
+});
+
+$('.menu-item.search').on('click', function() {
+    $('.image-room').hide();
+    $('.drop-down__inner.warehouse').hide();
+    $('.drop-down__inner.favourites').hide();
+    $('.drop-down__inner.search').show();
+});
+
+$('.image-cross').on('click', function() {
+    $('.drop-down__inner.warehouse').hide();
+    $('.drop-down__inner.favourites').hide();
+    $('.drop-down__inner.search').hide();
+    $('.image-room').show();
+});
+
+$('.slider').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true
 });
