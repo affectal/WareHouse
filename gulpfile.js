506ce3b49d76fb5dbe97e0c840289dc362162601
Affectal <aff.p@yandex.ru>
2020-04-15 19:27:44 +0700

    adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -9,46 +9,46 @@ let gulp = require('gulp'),
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/style.scss')
-    .pipe(sass({outputStyle: 'compressed'}))    
-    .pipe(rename({suffix : '.min'}))
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 8 versions']
-    }))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 versions']
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 
 gulp.task('style', function() {
     return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css'
-    ])
-    .pipe(concat('libs.min.css'))
-    .pipe(cssmin())
-    .pipe(gulp.dest('app/css'))
+            'node_modules/normalize.css/normalize.css',
+            'node_modules/slick-carousel/slick/slick.css'
+        ])
+        .pipe(concat('libs.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('app/css'))
 });
 
 
 gulp.task('script', function() {
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js'
-    ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+            'node_modules/slick-carousel/slick/slick.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
 });
 
 
 gulp.task('html', function() {
     return gulp.src('app/*.html')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 
 gulp.task('js', function() {
     return gulp.src('app/js*.js')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 
@@ -61,12 +61,11 @@ gulp.task('browser-sync', function() {
 });
 
 
-gulp.task('watch' , function() {
+gulp.task('watch', function() {
     gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
-}); 
+});
 
 
 gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
-
